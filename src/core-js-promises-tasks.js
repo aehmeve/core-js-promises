37710f1beeb4c72c5893df4afc4e9c4d29f728c5
aehmeve <ehmorizehere@gmail.com>
2024-05-01 05:28:20 +0900

    feat: finish all tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -96,8 +96,8 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
@@ -112,8 +112,15 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((promise) => {
+      if (promise.reason) {
+        return null;
+      }
+      return promise.value;
+    })
+  );
 }
 
 /**
@@ -134,8 +141,15 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let str = Promise.resolve('');
+  promises.forEach((promise) => {
+    str = str.then(async (res) => {
+      const value = await promise;
+      return res + value;
+    });
+  });
+  return str;
 }
 
 module.exports = {
